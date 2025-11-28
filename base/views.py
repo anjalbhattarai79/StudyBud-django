7323359a8495eb506eec338fd4615da229939c2b
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2025-11-28 09:13:42 +0545

    Implement Delete operation

--- base/views.py
+++ base/views.py
@@ -47,4 +47,8 @@ def update_room(request, pk):
     return render(request, 'base/room_form.html', context)
 
 def delete_room(request, pk):
-    return render(request, 'home')
+    room = Room.objects.get(id= pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj' : room})
