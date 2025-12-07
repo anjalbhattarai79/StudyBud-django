8c0ff86055fb142d7cb977667260df89441be100
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2025-12-07 20:37:38 +0545

    Implement static folder for image, css and javascript when needed

--- studybud/settings.py
+++ studybud/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+#STATIC_ROOT =
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
