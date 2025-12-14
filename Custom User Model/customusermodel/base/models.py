9afc9935696520df0d1252d5e364884d6ca09123
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2025-12-14 22:28:19 +0545

    Configuring custom user model

--- Custom User Model/customusermodel/base/models.py
+++ Custom User Model/customusermodel/base/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
