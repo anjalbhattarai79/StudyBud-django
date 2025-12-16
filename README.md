ce9efc70afa38d6fa35a01106eb8d590c6cd9320
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2025-12-16 12:30:56 +0545

    update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# StudyBud-django
-## This is an ongoing Project....
+# StudyBud-django 📚
+
+## Welcome to StudyBud! 🌟
+Hey there! StudyBud is a fun and easy way to connect with fellow learners and build study groups. Whether you're prepping for exams or diving into new topics, find buddies to chat and study together. No stress, just smart studying! 😊
+
+## What Makes It Awesome? ✨
+- **Find Study Buddies** 👫: Discover people with similar interests and join groups effortlessly.
+- **Share and Learn** 📖: Exchange tips, resources, and encouragement in a friendly community.
+- **Stay Motivated** 💪: Track your progress and celebrate wins with your study crew.
+
+This is an ongoing project, so stay tuned for more cool features! 🚀
--- base/views.py
+++ base/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 
 # Create your views here.
@@ -24,15 +23,15 @@ def loginPage(request):
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
         
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, "user does not exist !! ")
             
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         
         if user is not None:
             login(request, user)
@@ -51,9 +50,9 @@ def logoutPage(request):
     return redirect('home')
     
 def registerUser(request):
-    form = UserCreationForm() 
+    form = MyUserCreationForm() 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid:
             user = form.save(commit=False)
             user.username = user.username.lower()
